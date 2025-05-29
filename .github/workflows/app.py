ac6c00611b60fd12da320e5635eff99fb6db9238
hshshschoi <gotjrgofk@donga.ac.kr>
2025-05-30 08:55:06 +0900

    Update app.py

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "good"
+    return "Check"
 
 if __name__ == "__main__":
-    app.run()
+    # check out
+    app.run(host="0.0.0.0", port=8000)
