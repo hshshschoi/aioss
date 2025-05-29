5548420fe824b5d97564377d1a6d9170bba8962c
hshshschoi <gotjrgofk@donga.ac.kr>
2025-05-30 08:37:51 +0900

    Update app.py

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "✅ Hello from Azure Web App!"
+def home():
+    return "good"
 
 if __name__ == "__main__":
     app.run()
